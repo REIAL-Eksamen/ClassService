c6c0af782bf42d54fa057ff250dccfc69585b742
lukassloth <eaa24luhe@students.eaaa.dk>
2026-05-15 13:35:22 +0200

    Post-metoder

--- Controllers/ClassController.cs
+++ Controllers/ClassController.cs
@@ -1,9 +1,12 @@
 using ClassService.Models;
+using ClassService.DTO;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ClassService.Controllers;
 
-public class ClassController
+[ApiController]
+[Route("[controller]")]
+public class ClassController : ControllerBase
 {
     private static List<Class> _classes = new List<Class>()
     {
@@ -16,7 +19,7 @@ public class ClassController
             StartTime = new DateTime(2026, 05, 24, 14, 30, 0),
             EndTime = new DateTime(2026, 05, 24, 15, 25, 0),
             ClassCapacity = 34,
-            ClassStatus = true,
+            Status = ClassStatus.Scheduled ,
             Attendees = new List<string>()
             {
               "Lukas Sloth",
@@ -38,5 +41,32 @@ public class ClassController
     public Class? GetClassById(int ClassId)
     {
         return _classes.FirstOrDefault(c => c.ClassId == ClassId);
+    }
+    
+    [HttpPost("newscheduledclass")]
+    public ActionResult<Class> CreateNewScheduledClass([FromBody] Class? newClass)
+    {
+        if (newClass is null)
+            return BadRequest("Request body cannot be null.");
+        
+        return CreatedAtAction(nameof(CreateNewScheduledClass), newClass);
+    }
+
+    [HttpPost("createnewclass")]
+    public ActionResult<CreateNewClassDTO> MakeNewClass([FromBody] CreateNewClassDTO? newClassDto)
+    {
+        if (newClassDto is null)
+            return BadRequest("Request body cannot be null.");
+
+        Class newClass = new()
+        {
+            ClassName        = newClassDto.ClassName,
+            ClassDescription = newClassDto.ClassDescription,
+            ClassType        = newClassDto.ClassType,
+        };
+
+        // TODO: persist newClass to database, get generated ClassId back
+
+        return CreatedAtAction(nameof(MakeNewClass), CreateNewClassDTO.FromClass(newClass));
     }
 }
--- Models/Class.cs
+++ Models/Class.cs
@@ -1,15 +1,22 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ClassService.Models;
 
+public enum ClassStatus {Scheduled, Active, Cancelled, Done}
 public class Class
 {
     public int ClassId { get; set; }
-    public string? ClassName { get; set; }
-    public string? ClassDescription { get; set; }
-    public string? ClassType { get; set; }
+    [Required, MaxLength (50)]
+    public string ClassName { get; set; }
+    [Required, MaxLength(250)]
+    public string ClassDescription { get; set; }
+    [Required, MaxLength(50)]
+    public string ClassType { get; set; }
     public DateTime? StartTime { get; set; }
     public DateTime? EndTime { get; set; }
-    public int ClassCapacity { get; set; }
-    public bool ClassStatus { get; set; }
-    public List<string>? Attendees { get; set; }
-    public List<string>? Waitlist { get; set; }
+    [Range (0, 100)]
+    public int? ClassCapacity { get; set; }
+    public ClassStatus? Status { get; set; } =  ClassStatus.Scheduled;
+    public List<string>? Attendees { get; set; } = [];
+    public List<string>? Waitlist { get; set; } = [];
 }
